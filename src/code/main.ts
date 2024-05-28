c8b15f7e494833167fad48b35eea2a492ace03f7
Maysara <Maysara.Elshewehy@gmail.com>
2024-05-28 22:24:46 +0300

    up

--- src/code/main.ts
+++ src/code/main.ts
@@ -3,6 +3,8 @@
  * @description                             the main file (entry point)
  * @author                                  Maysara Elshewhy
  * @repo                                    https://github.com/Maysara-Elshewehy/mecs-npm-package
+ *
+ * @note                                    This script follows the `MECS` code style
 */
 
 
